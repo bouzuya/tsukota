0caec7e582412105cd19ae93dae0ad29572618c2
bouzuya <m@bouzuya.net>
2023-05-31 07:03:03 +0900

    Fix lint error in functions index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase-admin/app";
+import { type AppOptions, initializeApp } from "firebase-admin/app";
 import * as functions from "firebase-functions";
 import { defineString } from "firebase-functions/params";
 import { buildCreateCustomToken } from "./functions/create-custom-token";
@@ -9,15 +9,15 @@ const projectIdParams = defineString("PROJECT_ID");
 const bucketNameParams = defineString("BUCKET_NAME");
 
 const region = "asia-northeast2";
-const app = initializeApp(functions.config().firebase);
+const app = initializeApp(functions.config().firebase as AppOptions);
 // const app = initializeApp({ projectId: "demo-project" });
 
-exports.createCustomToken = buildCreateCustomToken(app, region);
+export const createCustomToken = buildCreateCustomToken(app, region);
 
-exports.scheduledFirestoreExport = buildScheduledFirestoreExport(
+export const scheduledFirestoreExport = buildScheduledFirestoreExport(
   region,
   projectIdParams,
   bucketNameParams
 );
 
-exports.storeAccountEvent = buildStoreAccountEvent(app, region);
+export const storeAccountEvent = buildStoreAccountEvent(app, region);
