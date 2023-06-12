b3b7f90eb6a1aa7bea17b8fcd9c9769056df15c0
bouzuya <m@bouzuya.net>
2023-06-13 06:48:58 +0900

    tsukota: Fix entry point

---
+++ packages/tsukota/App.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function Home(): JSX.Element {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
