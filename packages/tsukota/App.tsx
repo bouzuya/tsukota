43360d15568c24b2bc872ff51cce311ff4caa2f2
bouzuya <m@bouzuya.net>
2023-06-13 06:55:05 +0900

    tsukota: Add dummy AccountNew screen

--- packages/tsukota/App.tsx
+++ packages/tsukota/App.tsx
@@ -1,13 +1,44 @@
 import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  NavigationContainer,
+  useNavigation as RNNUseNavigation,
+} from "@react-navigation/native";
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
+const paramList = {
+  AccountNew: undefined,
+} as const;
+type ParamList = typeof paramList;
+
+const useNavigation = (): NativeStackNavigationProp<ParamList> => {
+  return RNNUseNavigation();
+};
+
 function Home(): JSX.Element {
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>Home</Text>
+      <Text
+        onPress={() => {
+          navigation.push("AccountNew");
+        }}
+      >
+        Home
+      </Text>
+    </View>
+  );
+}
+
+function AccountNew(): JSX.Element {
+  return (
+    <View>
+      <Text>AccountNew</Text>
     </View>
   );
 }
@@ -17,6 +48,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AccountNew" component={AccountNew} />
       </Stack.Navigator>
     </NavigationContainer>
   );
