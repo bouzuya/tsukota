922a32b4802216bac5a203662d683ee0090f49fd
bouzuya <m@bouzuya.net>
2023-07-01 11:13:20 +0900

    tsukota: Increment versionCode

--- packages/tsukota/app.config.ts
+++ packages/tsukota/app.config.ts
@@ -7,7 +7,7 @@ const expoConfig: ExpoConfig = {
       foregroundImage: "./assets/adaptive-icon-v2.png",
     },
     package: "net.bouzuya.lab.tsukota",
-    versionCode: 17,
+    versionCode: 18,
   },
   assetBundlePatterns: ["**/*"],
   extra: {
