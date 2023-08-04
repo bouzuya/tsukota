a5b3084a634c1e6c7270a4ae0aaaa663672c4d82
bouzuya <m@bouzuya.net>
2023-08-04 22:29:30 +0900

    usecases: Add AccountEventStore.load

--- packages/usecases/src/account-event-store.test.ts
+++ packages/usecases/src/account-event-store.test.ts
@@ -7,8 +7,10 @@ describe("AccountEventStore", () => {
       store: jest.fn().mockReturnValue(Promise.resolve()),
     } as unknown as AccountEventStore;
     const uid = "9fc3429c-9952-4cd5-9fe0-ab600169f189";
+    const accountId = "a7538bdd-ab22-4666-91ca-8284b8f2b671";
+    await accountEventStore.load(uid, accountId);
     await accountEventStore.store(uid, null, {
-      accountId: "a7538bdd-ab22-4666-91ca-8284b8f2b671",
+      accountId,
       at: "2020-01-02T03:04:05Z",
       id: "5f6a6518-c349-4c52-8b01-83611a49aaf9",
       name: "account name 1",
--- packages/usecases/src/account-event-store.ts
+++ packages/usecases/src/account-event-store.ts
@@ -1,6 +1,7 @@
 import type { AccountEvent } from "@bouzuya/tsukota-models";
 
 export type AccountEventStore = {
+  load(uid: string, accountId: string): Promise<AccountEvent[]>;
   store(
     uid: string,
     lastEventId: string | null,
