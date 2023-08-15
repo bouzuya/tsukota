87a51964635707af0f8f5d150d0b8c5118fb9b01
bouzuya <m@bouzuya.net>
2023-08-15 15:24:18 +0900

    tsukota: Fix to memoize TransactionListItem

--- packages/tsukota/src/components/TransactionList.tsx
+++ packages/tsukota/src/components/TransactionList.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
-import { FlatList, FlatListProps, View } from "react-native";
-import { List, Text } from "react-native-paper";
+import { FlatList, FlatListProps } from "react-native";
 import { Category, Transaction } from "../lib/account";
+import { TransactionListItem } from "./pages/TransactionIndex/TransactionListItem";
 
 type Props = Omit<
   FlatListProps<Transaction>,
@@ -32,51 +32,16 @@ export function TransactionList({
       {...props}
       data={transactions}
       keyExtractor={(item) => item.id}
-      renderItem={({ item: transaction }) => {
-        const ensureDescription = (s: string): string =>
-          s.length === 0 ? " " : s;
-        return (
-          <List.Item
-            description={ensureDescription(transaction.comment)}
-            key={transaction.id}
-            left={() => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  flexWrap: "nowrap",
-                  margin: 0,
-                  paddingStart: 16,
-                  position: "absolute",
-                  width: "100%",
-                }}
-              >
-                <Text style={{ flex: 1 }}>{transaction.date}</Text>
-                <Text
-                  style={{
-                    flex: 1,
-                    textAlign: "right",
-                    paddingHorizontal: 8,
-                  }}
-                >
-                  {categoryNames[transaction.categoryId]}
-                </Text>
-                <Text
-                  style={{
-                    flex: 1,
-                    textAlign: "right",
-                    paddingHorizontal: 8,
-                  }}
-                >
-                  {transaction.amount}
-                </Text>
-              </View>
-            )}
-            onLongPress={() => onLongPressTransaction(transaction)}
-            onPress={() => onPressTransaction(transaction)}
-            title=""
-          />
-        );
-      }}
+      renderItem={({ item: transaction }) => (
+        <TransactionListItem
+          {...{
+            categoryNames,
+            onLongPressTransaction,
+            onPressTransaction,
+            transaction,
+          }}
+        />
+      )}
       style={{ flex: 1, width: "100%" }}
     />
   );
---
+++ packages/tsukota/src/components/pages/TransactionIndex/TransactionListItem.tsx
@@ -0,0 +1,65 @@
+import { View } from "react-native";
+import { List, Text } from "react-native-paper";
+import { useMemo } from "react";
+import { Transaction } from "../../../lib/account";
+
+type Props = {
+  categoryNames: Record<string, string>;
+  onLongPressTransaction: (item: Transaction) => void;
+  onPressTransaction: (item: Transaction) => void;
+  transaction: Transaction;
+};
+
+export function TransactionListItem({
+  categoryNames,
+  onLongPressTransaction,
+  onPressTransaction,
+  transaction,
+}: Props): JSX.Element {
+  return useMemo(
+    () => (
+      <List.Item
+        description={
+          transaction.comment.length === 0 ? " " : transaction.comment
+        }
+        key={transaction.id}
+        left={() => (
+          <View
+            style={{
+              flexDirection: "row",
+              flexWrap: "nowrap",
+              margin: 0,
+              paddingStart: 16,
+              position: "absolute",
+              width: "100%",
+            }}
+          >
+            <Text style={{ flex: 1 }}>{transaction.date}</Text>
+            <Text
+              style={{
+                flex: 1,
+                textAlign: "right",
+                paddingHorizontal: 8,
+              }}
+            >
+              {categoryNames[transaction.categoryId]}
+            </Text>
+            <Text
+              style={{
+                flex: 1,
+                textAlign: "right",
+                paddingHorizontal: 8,
+              }}
+            >
+              {transaction.amount}
+            </Text>
+          </View>
+        )}
+        onLongPress={() => onLongPressTransaction(transaction)}
+        onPress={() => onPressTransaction(transaction)}
+        title=""
+      />
+    ),
+    [categoryNames, onLongPressTransaction, onPressTransaction, transaction],
+  );
+}
