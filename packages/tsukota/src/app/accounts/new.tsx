f19a4d2944af5983bc3ee4eb0fe9e85bd54bef18
bouzuya <m@bouzuya.net>
2023-08-19 22:40:48 +0900

    tsukota: Extract hooks from AccountNew page

--- packages/tsukota/src/app/accounts/new.tsx
+++ packages/tsukota/src/app/accounts/new.tsx
@@ -1,58 +1,12 @@
-import { err } from "neverthrow";
-import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import {
-  HeaderRightSaveButton,
-  Screen,
-  TextInput,
-  View,
-} from "../../components";
-import { useAccounts } from "../../components/AccountContext";
-import { useCurrentUserId } from "../../hooks/use-credential";
-import { createAccount, deps } from "../../lib/account";
-import { useTypedNavigation } from "../../lib/navigation";
-import { showErrorMessage } from "../../lib/show-error-message";
-
-type Form = {
-  name: string;
-};
+import React from "react";
+import { Screen, TextInput, View } from "../../components";
+import { useAccountNew } from "../../components/pages/AccountNew/hooks";
 
 export function AccountNew(): JSX.Element {
-  const {
-    control,
-    formState: { isSubmitting },
-    handleSubmit,
-  } = useForm<Form>({
-    defaultValues: {
-      name: "",
-    },
-  });
-  const { handleAccountCommand } = useAccounts();
-  const navigation = useTypedNavigation();
-  const { t } = useTranslation();
-  const currentUserId = useCurrentUserId();
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <HeaderRightSaveButton
-          isSubmitting={isSubmitting}
-          onPress={handleSubmit(onClickOk)}
-        />
-      ),
-    });
-  }, [isSubmitting, navigation]);
+  const { control, currentUserId, t } = useAccountNew();
 
   if (currentUserId === null) return <></>;
 
-  const onClickOk = async ({ name }: Form): Promise<void> => {
-    await handleAccountCommand(null, (oldAccount) =>
-      oldAccount !== null
-        ? err("account already exists")
-        : createAccount(deps, currentUserId, name),
-    ).match(() => navigation.goBack(), showErrorMessage);
-  };
   return (
     <Screen>
       <View style={{ flex: 1, width: "100%" }}>
---
+++ packages/tsukota/src/components/pages/AccountNew/hooks/index.tsx
@@ -0,0 +1,64 @@
+import { err } from "neverthrow";
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HeaderRightSaveButton } from "../../../../components";
+import { useAccounts } from "../../../../components/AccountContext";
+import { useCurrentUserId } from "../../../../hooks/use-credential";
+import { createAccount, deps } from "../../../../lib/account";
+import { useTypedNavigation } from "../../../../lib/navigation";
+import { showErrorMessage } from "../../../../lib/show-error-message";
+
+type Form = {
+  name: string;
+};
+
+export function useAccountNew(): {
+  control: ReturnType<typeof useForm<Form>>["control"];
+  currentUserId: ReturnType<typeof useCurrentUserId>;
+  t: ReturnType<typeof useTranslation>["t"];
+} {
+  const {
+    control,
+    formState: { isSubmitting },
+    handleSubmit,
+  } = useForm<Form>({
+    defaultValues: {
+      name: "",
+    },
+  });
+  const { handleAccountCommand } = useAccounts();
+  const navigation = useTypedNavigation();
+  const { t } = useTranslation();
+  const currentUserId = useCurrentUserId();
+
+  const onClickOk = useCallback(
+    async ({ name }: Form): Promise<void> => {
+      if (currentUserId === null)
+        throw new Error("assert currentUsesrId !== null");
+      await handleAccountCommand(null, (oldAccount) =>
+        oldAccount !== null
+          ? err("account already exists")
+          : createAccount(deps, currentUserId, name),
+      ).match(() => navigation.goBack(), showErrorMessage);
+    },
+    [currentUserId, handleAccountCommand, navigation],
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRightSaveButton
+          isSubmitting={isSubmitting}
+          onPress={handleSubmit(onClickOk)}
+        />
+      ),
+    });
+  }, [handleSubmit, isSubmitting, navigation, onClickOk]);
+
+  return {
+    control,
+    currentUserId,
+    t,
+  };
+}
