612b5954e690df039e6a7cdb9b060dc23630ec57
bouzuya <m@bouzuya.net>
2023-08-20 10:29:24 +0900

    tsukota: Extract hooks from OwnerNew page

--- packages/tsukota/src/app/accounts/[id]/owners/new.tsx
+++ packages/tsukota/src/app/accounts/[id]/owners/new.tsx
@@ -1,60 +1,12 @@
-import { err } from "neverthrow";
-import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import {
-  ActivityIndicator,
-  HeaderRightSaveButton,
-  Screen,
-  TextInput,
-  View,
-  useAccount,
-} from "../../../../components";
-import { addOwner, deps } from "../../../../lib/account";
-import { useTranslation } from "../../../../lib/i18n";
-import { useTypedNavigation, useTypedRoute } from "../../../../lib/navigation";
-import { showErrorMessage } from "../../../../lib/show-error-message";
-
-type Form = {
-  uid: string;
-};
+import React from "react";
+import { ActivityIndicator, Screen, TextInput, View } from "@/components";
+import { useOwnerNew } from "@/components/pages/OwnerNew/hooks";
 
 export function OwnerNew(): JSX.Element {
-  const navigation = useTypedNavigation();
-  const route = useTypedRoute<"OwnerNew">();
-  const { accountId } = route.params;
-  const { account, handleAccountCommand } = useAccount(accountId, []);
-  const {
-    control,
-    formState: { isSubmitting },
-    handleSubmit,
-  } = useForm<Form>({
-    defaultValues: {
-      uid: "",
-    },
-  });
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <HeaderRightSaveButton
-          isSubmitting={isSubmitting}
-          onPress={handleSubmit(onClickOk)}
-        />
-      ),
-    });
-  }, [isSubmitting, navigation]);
+  const { account, control, t } = useOwnerNew();
 
   if (account === null)
     return <ActivityIndicator size="large" style={{ flex: 1 }} />;
-
-  const onClickOk = async ({ uid }: Form): Promise<void> => {
-    await handleAccountCommand(account.id, (oldAccount) =>
-      oldAccount === null
-        ? err("account not found")
-        : addOwner(deps, oldAccount, uid),
-    ).match(() => navigation.goBack(), showErrorMessage);
-  };
 
   return (
     <Screen>
---
+++ packages/tsukota/src/components/pages/OwnerNew/hooks/index.tsx
@@ -0,0 +1,62 @@
+import { err } from "neverthrow";
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { HeaderRightSaveButton, useAccount } from "@/components";
+import { addOwner, deps } from "@/lib/account";
+import { useTranslation } from "@/lib/i18n";
+import { useTypedNavigation, useTypedRoute } from "@/lib/navigation";
+import { showErrorMessage } from "@/lib/show-error-message";
+
+type Form = {
+  uid: string;
+};
+
+export function useOwnerNew(): {
+  account: ReturnType<typeof useAccount>["account"];
+  control: ReturnType<typeof useForm<Form>>["control"];
+  t: ReturnType<typeof useTranslation>["t"];
+} {
+  const navigation = useTypedNavigation();
+  const route = useTypedRoute<"OwnerNew">();
+  const { accountId } = route.params;
+  const { account, handleAccountCommand } = useAccount(accountId, []);
+  const {
+    control,
+    formState: { isSubmitting },
+    handleSubmit,
+  } = useForm<Form>({
+    defaultValues: {
+      uid: "",
+    },
+  });
+  const { t } = useTranslation();
+
+  const onClickOk = useCallback(
+    async ({ uid }: Form): Promise<void> => {
+      if (account === null) throw new Error("assert account !== null");
+      await handleAccountCommand(account.id, (oldAccount) =>
+        oldAccount === null
+          ? err("account not found")
+          : addOwner(deps, oldAccount, uid),
+      ).match(() => navigation.goBack(), showErrorMessage);
+    },
+    [account, handleAccountCommand, navigation],
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRightSaveButton
+          isSubmitting={account === null || isSubmitting}
+          onPress={handleSubmit(onClickOk)}
+        />
+      ),
+    });
+  }, [account, handleSubmit, isSubmitting, navigation, onClickOk]);
+
+  return {
+    account,
+    control,
+    t,
+  };
+}
