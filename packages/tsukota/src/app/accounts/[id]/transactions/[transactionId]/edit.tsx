658a2274283e7b78a555c1ba86d5140004cc0ffb
bouzuya <m@bouzuya.net>
2023-08-20 10:40:23 +0900

    tsukota: Extract hooks from TransactionEdit page

--- packages/tsukota/src/app/accounts/[id]/transactions/[transactionId]/edit.tsx
+++ packages/tsukota/src/app/accounts/[id]/transactions/[transactionId]/edit.tsx
@@ -1,74 +1,14 @@
-import { err } from "neverthrow";
-import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
 import { View } from "react-native";
-import {
-  ActivityIndicator,
-  HeaderRightSaveButton,
-  Screen,
-  TransactionForm,
-  TransactionFormValues,
-  useAccount,
-} from "../../../../../components";
-import { deps, updateTransaction } from "../../../../../lib/account";
-import {
-  useTypedNavigation,
-  useTypedRoute,
-} from "../../../../../lib/navigation";
-import { showErrorMessage } from "../../../../../lib/show-error-message";
+import { ActivityIndicator, Screen, TransactionForm } from "@/components";
+import { useTransactionEdit } from "@/components/pages/TransactionEdit/hooks";
 
 export function TransactionEdit(): JSX.Element {
-  const navigation = useTypedNavigation();
-  const route = useTypedRoute<"TransactionEdit">();
-  const { accountId, amount, categoryId, comment, date, transactionId } =
-    route.params;
-  const { account, handleAccountCommand } = useAccount(accountId, []);
-  const {
-    control,
-    formState: { isSubmitting },
-    getValues,
-    handleSubmit,
-    setValue,
-  } = useForm<TransactionFormValues>({
-    defaultValues: {
-      amount,
-      categoryId,
-      comment: decodeURIComponent(comment),
-      date,
-    },
-  });
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <HeaderRightSaveButton
-          isSubmitting={isSubmitting}
-          onPress={handleSubmit(onClickOk)}
-        />
-      ),
-    });
-  }, [isSubmitting, navigation]);
+  const { account, accountId, control, getValues, setValue } =
+    useTransactionEdit();
 
   if (account === null)
     return <ActivityIndicator size="large" style={{ flex: 1 }} />;
-
-  const onClickOk = async ({
-    amount,
-    categoryId,
-    comment,
-    date,
-  }: TransactionFormValues): Promise<void> => {
-    await handleAccountCommand(account.id, (oldAccount) =>
-      oldAccount === null
-        ? err("account not found")
-        : updateTransaction(deps, oldAccount, transactionId, {
-            amount,
-            categoryId,
-            comment,
-            date,
-          }),
-    ).match(() => navigation.goBack(), showErrorMessage);
-  };
 
   return (
     <Screen>
---
+++ packages/tsukota/src/components/pages/TransactionEdit/hooks/index.tsx
@@ -0,0 +1,80 @@
+import { err } from "neverthrow";
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import {
+  HeaderRightSaveButton,
+  TransactionFormValues,
+  useAccount,
+} from "@/components";
+import { deps, updateTransaction } from "@/lib/account";
+import { useTypedNavigation, useTypedRoute } from "@/lib/navigation";
+import { showErrorMessage } from "@/lib/show-error-message";
+
+export function useTransactionEdit(): {
+  account: ReturnType<typeof useAccount>["account"];
+  accountId: string;
+  control: ReturnType<typeof useForm<TransactionFormValues>>["control"];
+  getValues: ReturnType<typeof useForm<TransactionFormValues>>["getValues"];
+  setValue: ReturnType<typeof useForm<TransactionFormValues>>["setValue"];
+} {
+  const navigation = useTypedNavigation();
+  const route = useTypedRoute<"TransactionEdit">();
+  const { accountId, amount, categoryId, comment, date, transactionId } =
+    route.params;
+  const { account, handleAccountCommand } = useAccount(accountId, []);
+  const {
+    control,
+    formState: { isSubmitting },
+    getValues,
+    handleSubmit,
+    setValue,
+  } = useForm<TransactionFormValues>({
+    defaultValues: {
+      amount,
+      categoryId,
+      comment: decodeURIComponent(comment),
+      date,
+    },
+  });
+
+  const onClickOk = useCallback(
+    async ({
+      amount,
+      categoryId,
+      comment,
+      date,
+    }: TransactionFormValues): Promise<void> => {
+      if (account === null) throw new Error("assert account !== null");
+      await handleAccountCommand(account.id, (oldAccount) =>
+        oldAccount === null
+          ? err("account not found")
+          : updateTransaction(deps, oldAccount, transactionId, {
+              amount,
+              categoryId,
+              comment,
+              date,
+            }),
+      ).match(() => navigation.goBack(), showErrorMessage);
+    },
+    [account, handleAccountCommand, navigation, transactionId],
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRightSaveButton
+          isSubmitting={account === null || isSubmitting}
+          onPress={handleSubmit(onClickOk)}
+        />
+      ),
+    });
+  }, [account, handleSubmit, isSubmitting, navigation, onClickOk]);
+
+  return {
+    account,
+    accountId,
+    control,
+    getValues,
+    setValue,
+  };
+}
