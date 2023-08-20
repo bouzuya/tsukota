c7777762a5f9fcf94e87e35fb11527c7eed6a75b
bouzuya <m@bouzuya.net>
2023-08-20 09:16:26 +0900

    tsukota: Extract hooks from CategoryEdit page

--- packages/tsukota/src/app/accounts/[id]/categories/[categoryId]/edit.tsx
+++ packages/tsukota/src/app/accounts/[id]/categories/[categoryId]/edit.tsx
@@ -1,64 +1,17 @@
-import { err } from "neverthrow";
-import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
+import React from "react";
 import {
   ActivityIndicator,
-  HeaderRightSaveButton,
   Screen,
   TextInput,
   View,
-  useAccount,
 } from "../../../../../components";
-import { deps, updateCategory } from "../../../../../lib/account";
-import {
-  useTypedNavigation,
-  useTypedRoute,
-} from "../../../../../lib/navigation";
-import { showErrorMessage } from "../../../../../lib/show-error-message";
-
-type Form = {
-  name: string;
-};
+import { useCategoryEdit } from "@/components/pages/CategoryEdit/hooks";
 
 export function CategoryEdit(): JSX.Element {
-  const navigation = useTypedNavigation();
-  const route = useTypedRoute<"CategoryEdit">();
-  const { accountId, categoryId, name } = route.params;
-  const nameDefault = decodeURIComponent(name);
-  const { account, handleAccountCommand } = useAccount(accountId, []);
-  const {
-    control,
-    formState: { isSubmitting },
-    handleSubmit,
-  } = useForm<Form>({
-    defaultValues: {
-      name: nameDefault,
-    },
-  });
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <HeaderRightSaveButton
-          isSubmitting={isSubmitting}
-          onPress={handleSubmit(onClickOk)}
-        />
-      ),
-    });
-  }, [isSubmitting, navigation]);
+  const { account, control, t } = useCategoryEdit();
 
   if (account === null)
     return <ActivityIndicator size="large" style={{ flex: 1 }} />;
-
-  const onClickOk = async ({ name }: Form): Promise<void> => {
-    await handleAccountCommand(account.id, (oldAccount) =>
-      oldAccount === null
-        ? err("account not found")
-        : updateCategory(deps, oldAccount, categoryId, name),
-    ).match(() => navigation.goBack(), showErrorMessage);
-  };
 
   return (
     <Screen>
---
+++ packages/tsukota/src/components/pages/CategoryEdit/hooks/index.tsx
@@ -0,0 +1,63 @@
+import { err } from "neverthrow";
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HeaderRightSaveButton, useAccount } from "@/components";
+import { deps, updateCategory } from "@/lib/account";
+import { useTypedNavigation, useTypedRoute } from "@/lib/navigation";
+import { showErrorMessage } from "@/lib/show-error-message";
+
+type Form = {
+  name: string;
+};
+
+export function useCategoryEdit(): {
+  account: ReturnType<typeof useAccount>["account"];
+  control: ReturnType<typeof useForm<Form>>["control"];
+  t: ReturnType<typeof useTranslation>["t"];
+} {
+  const navigation = useTypedNavigation();
+  const route = useTypedRoute<"CategoryEdit">();
+  const { accountId, categoryId, name } = route.params;
+  const nameDefault = decodeURIComponent(name);
+  const { account, handleAccountCommand } = useAccount(accountId, []);
+  const {
+    control,
+    formState: { isSubmitting },
+    handleSubmit,
+  } = useForm<Form>({
+    defaultValues: {
+      name: nameDefault,
+    },
+  });
+  const { t } = useTranslation();
+
+  const onClickOk = useCallback(
+    async ({ name }: Form): Promise<void> => {
+      if (account === null) throw new Error("assert account !== null");
+      await handleAccountCommand(account.id, (oldAccount) =>
+        oldAccount === null
+          ? err("account not found")
+          : updateCategory(deps, oldAccount, categoryId, name),
+      ).match(() => navigation.goBack(), showErrorMessage);
+    },
+    [account, categoryId, handleAccountCommand, navigation],
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRightSaveButton
+          isSubmitting={account === null || isSubmitting}
+          onPress={handleSubmit(onClickOk)}
+        />
+      ),
+    });
+  }, [account, handleSubmit, isSubmitting, navigation, onClickOk]);
+
+  return {
+    account,
+    control,
+    t,
+  };
+}
